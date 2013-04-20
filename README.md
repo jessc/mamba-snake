d39bc44715fbfa2965a83297dda5975cee969d22
Jesse Cummins <me@jessecummins.com>
2013-04-20 01:41:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Mamba Snake!
+=====
+
+A snake-like game with moving food that you must catch!
+
+- eat moving rabbits
+- imprison them with your body, then feast upon them
+- use venom?
+
---
+++ mambasnake.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+
+# 2013-04-20
+# Jesse Cummins
+# https://github.com/jessc
+# Credit to
+# https://github.com/mmorin/Snake-2
+# and
+# https://github.com/ThomasPedersen/rubysnake
+# for inspiration.
+
+
