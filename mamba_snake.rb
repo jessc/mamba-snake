1abc7f1f81aaa49c47ad4e1383b5bb4cdbb84f21
Jesse Cummins <me@jessecummins.com>
2013-06-06 17:56:01 -0400

    Updated variable name (@p1_killed to @p1_is_killed); added two_player bug list

--- mamba_snake.rb
+++ mamba_snake.rb
@@ -202,8 +202,8 @@ class MambaSnakeGame < Gosu::Window
   def new_game
     @time = 0
     @p1_dead = false unless @paused
-    @p1_killed = false
-    @p2_killed = false
+    @p1_is_killed = false
+    @p2_is_killed = false
     @map = Map.new(MAP_WIDTH, MAP_HEIGHT)
     unless TWO_PLAYER
       @p1 = Mamba.new(MAP_WIDTH, MAP_HEIGHT,
@@ -319,11 +319,11 @@ class MambaSnakeGame < Gosu::Window
     if TWO_PLAYER
       if snake_kill?(@p1, @p2)
         @p1_kills += 1
-        @p1_killed = true
+        @p2_is_killed = true
         restart_game
       elsif snake_kill?(@p2, @p1)
         @p2_kills += 1
-        @p2_killed = true
+        @p1_is_killed = true
         restart_game
       elsif snake_collide? @p2
         @p2_dead = true
@@ -345,8 +345,9 @@ class MambaSnakeGame < Gosu::Window
     draw_top_text
     draw_player_died("One") if @p1_dead
     draw_player_died("Two") if @p2_dead
-    draw_player_killed("One", "Two") if @p1_killed
-    draw_player_killed("Two", "One") if @p2_killed
+    p @p1_is_killed
+    draw_player_killed("One", "Two") if @p2_is_killed
+    draw_player_killed("Two", "One") if @p1_is_killed
     draw_bottom_text
 
     if TWO_PLAYER
@@ -445,3 +446,11 @@ end
 
 MambaSnakeGame.new.show
 
+=begin 
+
+two_player bugs:
+- kills for player_one going up
+- game pausing, resetting
+- it's not displaying that player_one killed player_two
+
+=end
